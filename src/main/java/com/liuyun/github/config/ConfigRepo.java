6fc8afdd04f01a445d29d56b1ac0fb356cce4969
liuyun <liuyunplus@gmail.com>
2020-11-20 16:49:15 +0800

    init

---
+++ src/main/java/com/liuyun/github/config/ConfigRepo.java
@@ -0,0 +1,17 @@
+package com.liuyun.github.config;
+
+/**
+ * @Author: liuyun
+ * @Date: 2019/3/8 下午4:45
+ */
+public class ConfigRepo {
+
+    /**
+     * 获取告警消息配置
+     * @return
+     */
+    public static AlarmConfig getAlarmConfig() {
+        return null;
+    }
+
+}
