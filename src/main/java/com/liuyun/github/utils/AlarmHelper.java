2712a16f14f62f84423f45abee33d8b14bf96fb2
liuyun <liuyunplus@gmail.com>
2020-11-22 18:05:58 +0800

    fix

--- src/main/java/com/liuyun/github/utils/AlarmHelper.java
+++ src/main/java/com/liuyun/github/utils/AlarmHelper.java
@@ -1,16 +1,23 @@
 package com.liuyun.github.utils;
 
 import com.liuyun.github.config.AlarmService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AlarmHelper {
 
     private static AlarmService alarmService;
 
     private static AlarmService getAlarmService() {
-        if(alarmService == null) {
-            alarmService = SpringBeanUtils.getBean(AlarmService.class);
+        try {
+            if(alarmService == null) {
+                alarmService = SpringBeanUtils.getBean(AlarmService.class);
+            }
+            return alarmService;
+        } catch (Exception e) {
+            log.info("告警消息推送服务不存在！");
+            return null;
         }
-        return alarmService;
     }
 
     public static void pushMsg(ErrorContext errorContext) {
